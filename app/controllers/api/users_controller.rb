8062d8d6a4e96d1ac68475a79fcf1f2a17d8ca37
Thanh Nguyen <tnguyenuni@gmail.com>
2016-12-09 16:17:51 +1100

    trying if request.post? with the create method

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,16 +8,18 @@ class Api::UsersController < ApplicationController
         render json: user
     end
     def create
-        user = User.new(user_params)
-        if user.save
-            render status: 200, json: {
-                message: "Successfully created new user",
-                user: user
-                }.to_json
-        else
-            render status: 422, json: {
-                errors: user.errors
-                }.to_json
+        if request.post?
+            user = User.new(user_params)
+            if user.save
+                render status: 200, json: {
+                    message: "Successfully created new user",
+                    user: user
+                    }.to_json
+            else
+                render status: 422, json: {
+                    errors: user.errors
+                    }.to_json
+            end
         end
     end
     def destroy
