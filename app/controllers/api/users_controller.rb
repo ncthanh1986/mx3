78b153f23e5713784ea71465a5f9e3ce47c1df82
Thanh Nguyen <tnguyenuni@gmail.com>
2016-12-09 12:43:52 +1100

    add basic users_controller api

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,48 @@
+class Api::UsersController < ApplicationController
+    skip_before_filter :verify_authenticity_token
+    def index
+        render json: User.all
+    end
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+    def create
+        user = User.new(user_params)
+        if user.save
+            render status: 200, json: {
+                message: "Successfully created new user",
+                user: user
+                }.to_json
+        else
+            render status: 422, json: {
+                errors: user.errors
+                }.to_json
+        end
+    end
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        render status: 200, json: {
+            message: "User deleted"
+        }.to_json
+    end
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+            render status: 200, json: {
+                message: "Successfully updated user's details",
+                user: user
+                }.to_json
+        else
+            render status: 500, json: {
+                message: "Cannot update user's details",
+                user: user
+                }.to_json
+        end
+    end
+    private
+    def user_params
+        params.require(:user).permit(:name,:email)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+    namespace :api do
+        resources :users
+    end
 	root 'application#hello'
 end
