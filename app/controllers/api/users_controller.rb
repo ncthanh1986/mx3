de4d0b8e480e180546888000970edff6eb57909c
Thanh Nguyen <tnguyenuni@gmail.com>
2016-12-09 15:40:54 +1100

    fix the user_params in the user_controller api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -43,6 +43,6 @@ class Api::UsersController < ApplicationController
     end
     private
     def user_params
-        params.require(:user).permit(:name,:email)
+        params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
 end
